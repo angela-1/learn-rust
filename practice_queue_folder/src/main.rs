97ebdf64fb60e606a02b831e700b4b5625338007
Angela-1 <ruoshui_engr@163.com>
2019-04-26 11:12:47 +0800

    update code.

--- practice_queue_folder/src/main.rs
+++ practice_queue_folder/src/main.rs
@@ -1,21 +1,16 @@
 use std::fs::{self, DirBuilder};
 use std::io::{stdin, Result};
 
-const QUEUE: &str = "foo";
-
-fn plus_one(x: Option<i32>) -> Option<i32> {
-    match x {
-        None => None,
-        Some(i) => Some(i + 1),
-    }
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
 
-fn get_queue_number() -> Option<i32> {
-    if let Ok(s) = fs::read_to_string(QUEUE) {
-        let number: Option<i32> = plus_one(s.parse().ok());
+fn get_queue_number(queue_file: &str) -> i32 {
+    if let Ok(s) = fs::read_to_string(queue_file) {
+        let number: i32 = plus_one(s.parse().unwrap_or(1));
         number
     } else {
-        None
+        1
     }
 }
 
@@ -25,21 +20,22 @@ fn make_numbered_directory(x: i32, name: &str) -> Result<()> {
         .create(x.to_string() + "_" + name + "/0-draft")
 }
 
-fn save_current_queue_number(x: i32) -> Result<()> {
-    fs::write(QUEUE, x.to_string())?;
+fn save_current_queue_number(queue_file: &str, x: i32) -> Result<()> {
+    fs::write(queue_file, x.to_string())?;
     Ok(())
 }
 
 fn main() {
-    println!("请输入名称：");
+    const QUEUE_FILE: &str = "foo";
+    println!("请输入任务名称：");
     let mut buffer = String::new();
     stdin().read_line(&mut buffer).unwrap();
     let name = buffer.trim();
 
-    let num: i32 = get_queue_number().unwrap_or(1);
+    let num: i32 = get_queue_number(QUEUE_FILE);
     match make_numbered_directory(num, &name[..]) {
         Ok(_) => {
-            save_current_queue_number(num).unwrap();
+            save_current_queue_number(QUEUE_FILE, num).unwrap();
             println!("完成");
         }
         Err(e) => println!("未创建目录 {}", e),
