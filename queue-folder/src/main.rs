cada30ce3be413d94443008310fb9287a2cf84b7
Angela <ruoshui_engr@163.com>
2020-02-13 21:15:25 +0800

    Add tests.

--- queue-folder/src/main.rs
+++ queue-folder/src/main.rs
@@ -1,14 +1,14 @@
-
 // 按顺序创建文件夹
-// 
+//
 use std::fs;
 use std::io::{stdin, Result};
 
+
 fn get_number(file: &str) -> i32 {
-	match fs::read_to_string(file) {
-		Ok(v) => v.parse().unwrap_or(1) + 1,
-		Err(_) => 1
-	}
+    match fs::read_to_string(file) {
+        Ok(v) => v.parse().unwrap_or(0) + 1,
+        Err(_) => 1,
+    }
 }
 
 fn save_number(file: &str, x: i32) -> Result<()> {
@@ -22,7 +22,7 @@ fn create_folder(x: i32, name: &str) -> Result<()> {
 }
 
 fn main() {
-	const QUEUE_FILE: &str = "foo";
+    const QUEUE_FILE: &str = "foo";
     println!("请输入任务名称：");
     let mut buffer = String::new();
     stdin().read_line(&mut buffer).unwrap();
@@ -31,10 +31,44 @@ fn main() {
     let x: i32 = get_number(QUEUE_FILE);
 
     match create_folder(x, name) {
-    	Ok(_) => save_number(QUEUE_FILE, x).unwrap(),
-    	Err(e) => println!("Error occured {}", e)
+        Ok(_) => save_number(QUEUE_FILE, x).unwrap(),
+        Err(e) => println!("发生错误 {}", e),
+    }
+    println!("完成 {}", x);
+}
+
+#[cfg(test)]
+mod unit_tests {
+    use super::*;
+    use std::path::Path;
+    #[test]
+    fn it_get_number_success() {
+        assert_eq!(get_number("test_foo") > 1, true);
     }
 
-    // save_number(QUEUE_FILE, x);
-    println!("Hello, world! {}", x);
+    #[test]
+    fn it_get_number_no_foo_and_return_one() {
+        assert_eq!(get_number("no_file") == 1, true);
+    }
+
+    #[test]
+    fn it_get_number_wrong_number_and_return_one() {
+        assert_eq!(get_number("test_foo_null") == 1, true);
+    }
+
+    #[test]
+    fn it_create_folder_success() {
+        create_folder(32, "test_create").unwrap();
+        assert_eq!(Path::new("32_test_create/0-draft").exists(), true);
+        fs::remove_dir_all("32_test_create").unwrap();
+    }
+
+    #[test]
+    fn it_save_number_success() {
+        use std::fs;
+        let test_file = "test_foo_save";
+        save_number(test_file, 22).unwrap();
+        assert_eq!(get_number(test_file) == 23, true);
+        fs::remove_file(test_file).unwrap();
+    }
 }
