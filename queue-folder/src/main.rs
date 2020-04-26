db539afccc6fecad68e8d0148cae9f78567fc2e8
angela-1 <angela-1@users.noreply.github.com>
2020-04-26 09:52:05 +0800

    update queue-folder.

--- queue-folder/src/main.rs
+++ queue-folder/src/main.rs
@@ -3,7 +3,7 @@
 use std::fs;
 use std::io::{stdin, Result};
 
-
+// 读取foo文件获取数字，如果有错则返回1
 fn get_number(file: &str) -> i32 {
     match fs::read_to_string(file) {
         Ok(v) => v.parse().unwrap_or(0) + 1,
@@ -11,15 +11,21 @@ fn get_number(file: &str) -> i32 {
     }
 }
 
-fn save_number(file: &str, x: i32) -> Result<()> {
-    fs::write(file, x.to_string())?;
+// 将当前数字存储到文件
+fn save_number(file: &str, x: i32) {
+    match fs::write(file, x.to_string()) {
+        Ok(_) => (),
+        Err(e) => println!("发生错误 {}", e),
+    }
+}
+
+// 创建任务文件夹模板
+fn create_folder(x: i32, name: &str) -> Result<()> {
+    fs::create_dir_all(x.to_string() + "_" + name + "/0-draft")?;
+    fs::create_dir_all(x.to_string() + "_" + name + "/1-来文")?;
     Ok(())
 }
 
-fn create_folder(x: i32, name: &str) -> Result<()> {
-    fs::create_dir_all(x.to_string() + "_" + name + "/0-draft")?;
-    Ok(())
-}
 
 fn main() {
     const QUEUE_FILE: &str = "foo";
@@ -31,8 +37,8 @@ fn main() {
     let x: i32 = get_number(QUEUE_FILE);
 
     match create_folder(x, name) {
-        Ok(_) => save_number(QUEUE_FILE, x).unwrap(),
-        Err(e) => println!("发生错误 {}", e),
+        Ok(_) => save_number(QUEUE_FILE, x),
+        Err(e) => println!("发生错误 {}", e)
     }
     println!("完成 {}", x);
 }
@@ -67,7 +73,7 @@ mod unit_tests {
     fn it_save_number_success() {
         use std::fs;
         let test_file = "test_foo_save";
-        save_number(test_file, 22).unwrap();
+        save_number(test_file, 22);
         assert_eq!(get_number(test_file) == 23, true);
         fs::remove_file(test_file).unwrap();
     }
